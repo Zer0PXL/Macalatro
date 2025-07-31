b6917527303ee7704d5b75bbed7659f4b882b328
emm <emm@notAnEmail>
2025-07-31 15:53:56 +0300

    Enhance MAGIC enum with detailed comments
    Updated the `MAGIC` enum in `Consumables.hpp` to include descriptive comments for each enumerator. This change improves code readability and clarifies the functionality of each magic type.

--- Macalatro/Consumables.hpp
+++ Macalatro/Consumables.hpp
@@ -4,25 +4,25 @@ struct GameState;
 
 enum MAGIC
 {
-	EXTRA,
-	GLUETUBE,
-	LOVE,
-	HATE,
-	RICH,
-	TREE,
-	MORE,
-	LESS,
-	SWORD,
-	SPEAR,
-	SHIELD,
-	PEEKABOO,
-	BLANK,
-	LOSTCRAYON,
-	RIP,
-	WILDACE,
-	ADOLLAH,
-	COINFLIP,
-	SMOKEBOMB
+	EXTRA, // Bonus score for playing
+	GLUETUBE, // Discards random card along with the card played
+	LOVE, // Turns 2 cards into Hearts
+	HATE, // Turns 2 cards into Spades
+	RICH, // Turns 2 cards into Diamonds
+	TREE, // Turns 2 cards into Clubs
+	MORE, // Increases the rank of 2 cards
+	LESS, // Decreases the rank of 3 cards
+	SWORD, // Turns a card into a Draw 1 card
+	SPEAR, // Turns a card into a Draw 2 card
+	SHIELD, // Turns a card into a Skip card
+	PEEKABOO, // See the top 3 cards in your deck
+	BLANK, // Use last used MAGIC card
+	LOSTCRAYON, // Turn one card into another
+	RIP, // Rip a card
+	WILDACE, // Playable at any time to change suit
+	ADOLLAH, // Gives 10 coins
+	COINFLIP, // 1 in 2 chance to give 20 coins
+	SMOKEBOMB // Playable at any time to skip the AI
 };
 
 class Consumables
